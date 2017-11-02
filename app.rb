074162f7326b1407bcbae4144678d91d103ca029
Juri Matsuda <matsuda.juri@gmail.com>
2017-11-02 16:57:47 +0900

    jsファイル追加

--- app.rb
+++ app.rb
@@ -3,4 +3,15 @@ require "sinatra/reloader"
 
 class App < Sinatra::Base
 
+  get '/' do
+    slim :index
+  end
+
+  get '/member/:name' do
+    member_age = rand(100)
+    content_type :json
+    data = {name: params[:name], age: member_age}
+    data.to_json
+  end
+
 end
